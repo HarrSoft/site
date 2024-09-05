8ccd0ace635ef31a36eeb67f6229d53559f2c3cb
Ahmed Zaher <82454482+JadeZaher@users.noreply.github.com>
2024-09-05 07:24:26 -0600

    spline - tailwind css - initial site structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script>
-	import Header from './Header.svelte';
-	import '../app.css';
+	import "../app.css";
+
 </script>
 
-<div class="app">
-	<Header />
+<svelte:head>
+	<title>HarrSoft Studio</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
 
+<div class="app">
 	<main>
 		<slot />
 	</main>
@@ -16,38 +19,5 @@
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
+	
 </style>
