63a32f7cc101e0006179bf5bc30e174df7ef2778
JadeZaher <contact@ahmedzaher.net>
2025-01-20 20:19:25 -0700

    improve loading state

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import Nav from '$com/nav.svelte';
 	import { isModalOpen } from '$lib/stores/modal';
+	import { fade } from 'svelte/transition';
 </script>
 
 <svelte:head>
@@ -9,7 +10,10 @@
 	<meta name="description" content="We Build." />
 </svelte:head>
 
-<div class="min-h-screen w-full bg-white {$isModalOpen ? 'pr-[17px]' : ''} overflow-x-clip">
+<div
+	transition:fade={{ duration: 200 }}
+	class="min-h-screen w-full bg-white {$isModalOpen ? 'pr-[17px]' : ''} overflow-x-clip"
+>
 	<Nav />
 	<main class="bg-white pt-14">
 		<slot />
