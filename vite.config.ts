b345c5238853a1bf3a4923138f17322e3be8de89
Lavra Tamutus <lavratamutus@gmail.com>
2026-08-24 13:09:19 -0400

    Mess around to hopefully clear out tailwind error crashing builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { enhancedImages } from '@sveltejs/enhanced-img';
-
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), enhancedImages()],
+	plugins: [sveltekit(), tailwindcss()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
